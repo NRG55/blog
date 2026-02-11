a6de541b9893efea12c7709f663d30fae39fc0a8
NRG55 <maloj_1980@yahoo.fr>
2026-02-11 10:17:37 +0000

    chore(blog-client-public): init (install vite, react, tailwindcss)

---
+++ blog-client-public/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <div class="text-red-500">
+                Test   
+            </div>
+        
+        </>
+    )
+}
+
+export default App
