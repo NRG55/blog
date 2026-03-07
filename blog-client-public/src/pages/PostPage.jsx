b2b16f07e6b2f32e4046496db3f228dee3dd9351
NRG55 <maloj_1980@yahoo.fr>
2026-03-07 13:24:14 +0000

    feat(blog-client-public): update PostPage - add NotFoundPage handling and restructure loading logic

--- blog-client-public/src/pages/PostPage.jsx
+++ blog-client-public/src/pages/PostPage.jsx
@@ -4,19 +4,11 @@ import { useEffect, useState } from "react";
 import AnimationWrapper from "../components/AnimationWrapper";
 import formatDate from "../utils/formatDate";
 import CommentSection from "../components/CommentSection";
+import NotFoundPage from "./NotFoundPage";
 
 const PostPage = () => {
-    const [ loading, setLoading ] = useState(false);
-    const [ post, setPost ] = useState({
-                                            id: '',
-                                            title: '',
-                                            body: '',
-                                            createdAt: '',
-                                            author: { username: '' },
-                                        });
-
-    const { id, title, body, imageUrl, createdAt, author: { username } } = post;
-
+    const [ loading, setLoading ] = useState(true);
+    const [ post, setPost ] = useState(null);
     const { slug } = useParams();
 
     const getPost = async () => {
@@ -38,16 +30,22 @@ const PostPage = () => {
         getPost();
     }, []);
 
+    if (loading) {
+        return <p className="p-20 text-center">Loading...</p>;
+    };
+
+    if (!post) {
+        return <NotFoundPage />;
+    };
+
+    const { id, title, body, imageUrl, createdAt, author: { username } } = post;    
+
     return (
         <AnimationWrapper>
-            {
-                loading
-                ?
-                <p>Loading...</p>
-                :
+            {                
                 <div className="grow flex-col max-w-225 block mx-auto py-10 max-lg:px-[5vw]">
                     <div className="">
-                        <img src={ imageUrl } className="aspect-video"/>
+                        <img src={ imageUrl || null } className="aspect-video"/>
 
                         <div className="mt-12">
                             <h2 className="text-3xl font-medium">{ title }</h2>
